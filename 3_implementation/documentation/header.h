712cb6fc8552f2a55fa255985d9ab6397ef01601
KARTIKHULLUR <karthikr05112000@gmail.com>
2022-02-11 13:56:17 +0530

    update

---
+++ 3_implementation/documentation/header.h
@@ -0,0 +1,23 @@
+/**
+ * @file header.h
+ * @author KARTIK HULLUR (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+// Header Guard
+#ifndef header_H_
+#define header_H_
+
+// include files
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+#endif
+
+
